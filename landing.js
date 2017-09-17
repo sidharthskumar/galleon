f4481973fdc7054437428cde84bf09165c4ecf4d
Chaldron <moonsketch@me.com>
2017-09-17 07:41:59 -0400

    Added google auth pt1

--- landing.js
+++ landing.js
@@ -126,6 +126,34 @@ $(function()
             });
     });
 
+    $("#google-button").click(function()
+    {
+        console.log('clicked');
+        var provider = new firebase.auth.GoogleAuthProvider();
+        firebase.auth().getRedirectResult().then(function(result) {
+
+          if (result.credential) {
+            // This gives you a Google Access Token. You can use it to access the Google API.
+            var token = result.credential.accessToken;
+            console.log(token);
+            // ...
+          }
+          // The signed-in user info.
+          var user = result.user;
+        }).catch(function(error) {
+          // Handle Errors here.
+          var errorCode = error.code;
+          var errorMessage = error.message;
+          // The email of the user's account used.
+          var email = error.email;
+          // The firebase.auth.AuthCredential type that was used.
+          var credential = error.credential;
+          console.log(error);
+          // ...
+        });
+
+    });
+
    
 
 
